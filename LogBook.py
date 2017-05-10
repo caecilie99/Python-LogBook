29640dec41e4297ae46f239191d98494fb5e3445
caecilie99 <birgit.reiter@stud.fh-luebeck.de>
2017-05-10 21:57:45 +0200

    setup treeview

--- LogBook.py
+++ LogBook.py
@@ -14,7 +14,7 @@ class LogBookWindow(gtk.Window):
 
     def __init__(self):
         gtk.Window.__init__(self)
-        gtk.Window.set_title(self, "Dat issen Test")
+        gtk.Window.set_title(self, "Logbuch des Captains")
         gtk.Window.set_default_size(self, 600, 400)
         self.set_border_width(10)
         self.mainbox = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
@@ -28,15 +28,15 @@ class LogBookWindow(gtk.Window):
 
         # create label
         self.label = gtk.Label()
-        self.label.set_markup("<b>Your entry</b>")
-        self.rightbox.pack_start(self.label, True, True, 0)
+        self.label.set_markup("<b>Neuer Eintrag</b>")
+        self.leftbox.pack_start(self.label, True, True, 0)
 
         self.entry = gtk.Entry()
         self.leftbox.pack_start(self.entry, True, True, 0)
 
         # create button1
         self.button1 = gtk.Button(label="Save")
-        self.button1.connect('clicked', self.change_label, self.entry)
+        self.button1.connect('clicked', self.__change_label)
         self.leftbox.pack_start(self.button1, True, True, 0)
 
         # create button2
@@ -44,8 +44,27 @@ class LogBookWindow(gtk.Window):
         self.button2.connect("clicked", gtk.main_quit)
         self.leftbox.pack_start(self.button2, True, True, 0)
 
-    def change_label(self):
-        self.label.set_markup(self.entry.g)
+        self.store = gtk.ListStore(str, str)
+        self.store.append(("First", "Entry 1"))
+        self.store.append(("Second", "Entry 2"))
+        self.store.append(("Third", "Entry 3"))
+
+        self.__create_view()
+
+    def __create_view(self):
+        self.view = gtk.TreeView(self.store)
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn('Sternzeit', renderer, text=0)
+        column.set_sort_column_id(0)
+        self.view.append_column(column)
+
+        column = gtk.TreeViewColumn('Eintrag', renderer, text=1)
+        self.view.append_column(column)
+
+        self.rightbox.pack_start(self.view, True, True, 0)
+
+    def __change_label(self, widget):
+        self.store.append(("neu", self.entry.get_text()))
 
 
 logBookWindow = LogBookWindow()
