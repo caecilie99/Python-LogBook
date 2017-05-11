43566e7047be0b9133eb5c1b59e591c16b172833
caecilie99 <birgit.reiter@stud.fh-luebeck.de>
2017-05-11 21:13:35 +0200

    setup gridlayout

--- LogBook.py
+++ LogBook.py
@@ -17,32 +17,26 @@ class LogBookWindow(gtk.Window):
         gtk.Window.set_title(self, "Dat issen Test")
         gtk.Window.set_default_size(self, 600, 400)
         self.set_border_width(10)
-        self.mainbox = gtk.Box(orientation=gtk.Orientation.HORIZONTAL)
-        self.add(self.mainbox)
-
-        self.leftbox = gtk.Box(orientation=gtk.Orientation.VERTICAL)
-        self.mainbox.pack_start(self.leftbox, True, True, 0)
-
-        self.rightbox = gtk.Box(orientation=gtk.Orientation.VERTICAL)
-        self.mainbox.pack_start(self.rightbox, True, True, 0)
+        self.mainGrid = gtk.Grid()
+        self.add(self.mainGrid)
 
         # create label
         self.label = gtk.Label()
         self.label.set_markup("<b>Your entry</b>")
-        self.rightbox.pack_start(self.label, True, True, 0)
+        self.mainGrid.attach(self.label)
 
         self.entry = gtk.Entry()
-        self.leftbox.pack_start(self.entry, True, True, 0)
+        self.mainGrid.attach(self.entry)
 
         # create button1
         self.button1 = gtk.Button(label="Save")
         self.button1.connect('clicked', self.change_label, self.entry)
-        self.leftbox.pack_start(self.button1, True, True, 0)
+        self.mainGrid.attach(self.button1)
 
         # create button2
         self.button2 = gtk.Button(label="Quit")
         self.button2.connect("clicked", gtk.main_quit)
-        self.leftbox.pack_start(self.button2, True, True, 0)
+        self.mainGrid.attach(self.button2)
 
     def change_label(self):
         self.label.set_markup(self.entry.g)
